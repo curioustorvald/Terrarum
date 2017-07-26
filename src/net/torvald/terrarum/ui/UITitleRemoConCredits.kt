5c2b1e75864de67119db7a4c4b0bd2615b65b6ac
minjaesong <alswo9628@gmail.com>
2017-07-26 17:03:22 +0900

    RemoCons fully working; also fixed my stupidity

--- src/net/torvald/terrarum/ui/UITitleRemoConCredits.kt
+++ src/net/torvald/terrarum/ui/UITitleRemoConCredits.kt
@@ -3,6 +3,8 @@ package net.torvald.terrarum.ui
 import com.badlogic.gdx.graphics.Camera
 import com.badlogic.gdx.graphics.Color
 import com.badlogic.gdx.graphics.g2d.SpriteBatch
+import net.torvald.terrarum.CreditSingleton
+import net.torvald.terrarum.Terrarum
 import net.torvald.terrarum.gameactors.Second
 
 class UITitleRemoConCredits(val superMenu: UICanvas) : UICanvas() {
@@ -20,9 +22,20 @@ class UITitleRemoConCredits(val superMenu: UICanvas) : UICanvas() {
     override var openCloseTime: Second = 0f
 
 
+    private val textAreaHMargin = 48
+    private val textAreaWidth = (Terrarum.WIDTH * 0.75).toInt()
+    private val textAreaHeight = Terrarum.HEIGHT - textAreaHMargin * 2
+    private val textArea = UIItemTextArea(this,
+            Terrarum.WIDTH - textAreaWidth, textAreaHMargin,
+            textAreaWidth, textAreaHeight
+    )
+    private var drawTextArea = true
+
+
     private val menubar = UIItemTextButtonList(
             this,
             menuLabels,
+            0, UITitleRemoConRoot.menubarOffY,
             this.width, this.height,
             textAreaWidth = this.width,
             readFromLang = true,
@@ -30,26 +43,55 @@ class UITitleRemoConCredits(val superMenu: UICanvas) : UICanvas() {
             highlightBackCol = Color(0),
             backgroundCol = Color(0),
             inactiveCol = Color.WHITE,
-            defaultSelection = null
+            defaultSelection = 0 // show CREDITS
     )
 
     init {
         uiItems.add(menubar)
+        uiItems.add(textArea)
+
+
+
+        ////////////////////////////
+
 
 
         // attach listeners
         menubar.buttons[menuLabels.indexOf("MENU_LABEL_RETURN")].clickOnceListener = { _, _, _ ->
+            menubar.selectedIndex = menubar.defaultSelection
             this.setAsClose()
             superMenu.setAsOpen()
+        }
+
+        menubar.selectionChangeListener = { _, newIndex ->
+            textArea.scrollPos = 0
+
+            if (newIndex == menuLabels.indexOf("MENU_LABEL_CREDITS")) {
+                textArea.setWallOfText(CreditSingleton.credit)
+                drawTextArea = true
+            }
+            else if (newIndex == menuLabels.indexOf("MENU_CREDIT_GPL_DNT")) {
+                textArea.setWallOfText(CreditSingleton.gpl3)
+                drawTextArea = true
+            }
+            else {
+                drawTextArea = false
+            }
         }
     }
 
     override fun updateUI(delta: Float) {
         menubar.update(delta)
+        if (drawTextArea) {
+            textArea.update(delta)
+        }
     }
 
     override fun renderUI(batch: SpriteBatch, camera: Camera) {
         menubar.render(batch)
+        if (drawTextArea) {
+            textArea.render(batch)
+        }
     }
 
     override fun doOpening(delta: Float) {
---
+++ work_files/GameDesign/TITLE_SCREEN.md
@@ -0,0 +1,81 @@
+# Main
+
+## Scenery
+
+Simulation of the world, day and night fast, camera follows landscape
+
+## RemoCon
+- Singleplayer
+- Options
+- Modules
+- Language
+- Credits
+- Quit
+
+
+# Singleplayer
+
+## RemoCon
+- New Character
+- Delete Character
+- Name
+- Return
+
+## Panel
+- Character selection screen, limited to 8 entries
+- 8 Cells made of semitransparent dark rects
+
+### Persona Cell
+- Icon on the left
+- Two rows:
+    + Name --------- | Creation date | Last play date | Total play time
+    + Name of the current universe | Number of multiverses (if >= 2)
+
+
+# Options
+
+## RemoCon
+- Controls
+- Graphics
+- Gameplay Options
+- Return
+
+
+# Modules
+
+## RemoCon
+- Return
+
+## Panel
+- List of module info cell
+
+### Module Info Cell
+- Loadorder number on the left
+- Two rows:
+    + Name (propername) ------- | version | Author
+    + Description ------------- | releasedate in YYYY-MM-DD
+
+
+# Languages
+
+## RemoCon
+- Return
+
+## Panel
+- List of languages available, click to change immediately
+
+
+# Credits
+
+## RemoCon
+- Credits
+- GPL
+- Back
+
+## Panel
+- Display selected menu, just a wall of text
+
+
+# Quit
+
+Execute command ```System.exit(0)``` (terminate application immediately)
