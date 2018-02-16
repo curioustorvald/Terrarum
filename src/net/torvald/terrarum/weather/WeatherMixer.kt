d11fd281f41447f6b59192d53954b7040e418988
minjaesong <alswo9628@gmail.com>
2018-02-16 10:49:13 +0900

    colourutil update

--- src/net/torvald/terrarum/weather/WeatherMixer.kt
+++ src/net/torvald/terrarum/weather/WeatherMixer.kt
@@ -168,7 +168,7 @@ object WeatherMixer {
         // interpolate R, G, B and A
         val scale = (timeInSec % dataPointDistance).toFloat() / dataPointDistance // [0.0, 1.0]
 
-        val newCol = CIELabUtil.getGradient(scale, colourThis, colourNext)
+        val newCol = CIELuvUtil.getGradient(scale, colourThis, colourNext)
 
         /* // very nice monitor code
         // 65 -> 66 | 300 | 19623 | RGB8(255, 0, 255) -[41%]-> RGB8(193, 97, 23) | * `230`40`160`
