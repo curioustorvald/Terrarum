ef8285dd9771b9626b78138ae540ac71ccf4f495
minjaesong <alswo9628@gmail.com>
2018-09-16 03:32:12 +0900

    serialised RNG; font update

---
+++ src/net/torvald/terrarum/modulebasegame/ui/UIItemSavegameInfoCell.kt
@@ -0,0 +1,32 @@
+package net.torvald.terrarum.modulebasegame.ui
+
+import com.badlogic.gdx.graphics.Camera
+import com.badlogic.gdx.graphics.g2d.SpriteBatch
+import net.torvald.terrarum.Terrarum
+import net.torvald.terrarum.ui.UICanvas
+import net.torvald.terrarum.ui.UIItem
+import java.io.File
+
+/**
+ * @param savefile TEVd file
+ *
+ * Created by minjaesong on 2018-09-15.
+ */
+class UIItemSavegameInfoCell(
+        parent: UICanvas,
+        savefile: File,
+        override val width: Int,
+        override var posX: Int,
+        override var posY: Int
+) : UIItem(parent) {
+
+    override val height: Int = Terrarum.fontGame.lineHeight.toInt() * 2
+
+    override fun render(batch: SpriteBatch, camera: Camera) {
+
+    }
+
+    override fun dispose() {
+
+    }
+}
