45f07c88b2dd354958aa755232fc37ac966f7371
minjaesong <alswo9628@gmail.com>
2019-01-07 04:13:38 +0900

    first successful sprite assembly ALL kwd still not implemented

--- src/net/torvald/terrarum/tests/SpriteAssemblerTest.kt
+++ src/net/torvald/terrarum/tests/SpriteAssemblerTest.kt
@@ -1,7 +1,7 @@
 package net.torvald.terrarum.tests
 
 import net.torvald.spriteassembler.ADProperties
-import net.torvald.spriteassembler.AssembleFrameAWT
+import net.torvald.spriteassembler.AssembleFramePixmap
 import java.io.StringReader
 
 /**
@@ -11,7 +11,7 @@ class SpriteAssemblerTest {
 
     operator fun invoke() {
         val properties = ADProperties(StringReader(ADLParsingTest().TEST_STR))
-        AssembleFrameAWT.invoke(properties, "ANIM_RUN_1")
+        AssembleFramePixmap.invoke(properties, "ANIM_RUN_1")
     }
 
 }
