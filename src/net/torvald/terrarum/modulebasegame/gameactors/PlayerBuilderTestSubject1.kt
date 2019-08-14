8a4bc986573ebee26065fa77e454e2cf5d6fcc99
minjaesong <alswo9628@gmail.com>
2019-08-15 00:20:42 +0900

    shitty implementaion of "air jumping"

--- src/net/torvald/terrarum/modulebasegame/gameactors/PlayerBuilderTestSubject1.kt
+++ src/net/torvald/terrarum/modulebasegame/gameactors/PlayerBuilderTestSubject1.kt
@@ -43,6 +43,8 @@ object PlayerBuilderTestSubject1 {
         //p.actorValue[AVKey.LUMB] = 1.37
         //p.actorValue[AVKey.LUMA] = 1.93
 
+        p.actorValue[AVKey.AIRJUMPPOINT] = 0
+
         return p
     }
 }
