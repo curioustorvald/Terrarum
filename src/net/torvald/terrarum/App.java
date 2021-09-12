af954b8c390b1f9fff4013454e57de5a524facec
minjaesong <alswo9628@gmail.com>
2021-09-13 02:11:29 +0900

    this seems to be optimal font cache size for the game

--- src/net/torvald/terrarum/App.java
+++ src/net/torvald/terrarum/App.java
@@ -461,7 +461,7 @@ public class App implements ApplicationListener {
 
 
         fontGame = new GameFontBase(FONT_DIR, false, true,
-                false, Texture.TextureFilter.Nearest, Texture.TextureFilter.Nearest, false, 256, false
+                false, Texture.TextureFilter.Nearest, Texture.TextureFilter.Nearest, false, 64, false
         );
         Lang.invoke();
 
