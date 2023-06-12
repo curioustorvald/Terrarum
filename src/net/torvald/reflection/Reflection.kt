0599ce91b1631ef46a73234354ae9beaec9b0808
minjaesong <alswo9628@gmail.com>
2023-06-12 15:08:42 +0900

    reflection function update

--- src/net/torvald/reflection/Reflection.kt
+++ src/net/torvald/reflection/Reflection.kt
@@ -3,15 +3,15 @@ package net.torvald.reflection
 /**
  * Created by minjaesong on 2023-03-25.
  */
-fun Any.extortField(name: String): Any? { // yes I'm deliberately using negative words for the function name
+inline fun <reified T> Any.extortField(name: String): T? { // yes I'm deliberately using negative words for the function name
     return this.javaClass.getDeclaredField(name).let {
         it.isAccessible = true
-        it.get(this)
+        it.get(this) as T?
     }
 }
-fun Any.forceInvoke(name: String, params: Array<Any>): Any? { // yes I'm deliberately using negative words for the function name
+inline fun <reified T> Any.forceInvoke(name: String, params: Array<Any>): T? { // yes I'm deliberately using negative words for the function name
     return this.javaClass.getDeclaredMethod(name, *(params.map { it.javaClass }.toTypedArray())).let {
         it.isAccessible = true
-        it.invoke(this, *params)
+        it.invoke(this, *params) as T?
     }
 }
