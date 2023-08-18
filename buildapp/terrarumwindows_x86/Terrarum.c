9a90bf69d4debd0ebbf4ba09a2f73ce20438ca2d
minjaesong <alswo9628@gmail.com>
2023-08-18 10:57:40 +0900

    trying to use the proper method of running exe on exe

--- buildapp/terrarumwindows_x86/Terrarum.c
+++ buildapp/terrarumwindows_x86/Terrarum.c
@@ -1,6 +1,24 @@
+#include <windows.h>
 #include <stdio.h>
-#include <stdlib.h>
+#include <tchar.h>
 
 int main() {
-    return system(".\\out\\runtime-windows-x86\\bin\\Terrarum.exe -jar .\\out\\TerrarumBuild.jar");
+    STARTUPINFOW si;
+    PROCESS_INFORMATION pi;
+
+    ZeroMemory(&si, sizeof(si));
+    si.cb = sizeof(si);
+    ZeroMemory(&pi, sizeof(pi));
+
+    if (CreateProcessW(
+            ".\\out\\runtime-windows-x86\\bin\\Terrarum.exe -jar .\\out\\TerrarumBuild.jar",
+            NULL, NULL, NULL, FALSE, CREATE_NO_WINDOW, NULL, NULL, &si, &pi)) {
+
+        WaitForSingleObject(pi.hProcess, INFINITE);
+        CloseHandle(pi.hProcess);
+        CloseHandle(pi.hThread);
+    }
+
+    return 0;
+    //return system(".\\out\\runtime-windows-x86\\bin\\Terrarum.exe -jar .\\out\\TerrarumBuild.jar");
 }
