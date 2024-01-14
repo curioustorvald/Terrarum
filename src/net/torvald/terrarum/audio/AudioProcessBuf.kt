7aa376dc6930301c0011cf11f89343010a0f9546
minjaesong <alswo9628@gmail.com>
2024-01-14 19:45:25 +0900

    fix: condition for do resampling

--- src/net/torvald/terrarum/audio/AudioProcessBuf.kt
+++ src/net/torvald/terrarum/audio/AudioProcessBuf.kt
@@ -35,7 +35,7 @@ class AudioProcessBuf(val inputSamplingRate: Int, val audioReadFun: (ByteArray) 
         get() = inputSamplingRate * playbackSpeed
 
     private val doResample
-        get() = (internalSamplingRate - SAMPLING_RATE).absoluteValue >= 0.5f
+        get() = inputSamplingRate == SAMPLING_RATE && (playbackSpeed - 1f).absoluteValue < (1f / 1024f)
 
     companion object {
         private val epsilon: Double = Epsilon.E
