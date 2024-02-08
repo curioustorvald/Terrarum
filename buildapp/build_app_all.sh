a1ef80b2c05d8931d870cb80507f248faa8a67ce
minjaesong <alswo9628@gmail.com>
2024-02-09 00:32:17 +0900

    build script update

--- buildapp/build_app_all.sh
+++ buildapp/build_app_all.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
-./build_app_linux_arm.sh
 ./build_app_linux_x86.sh
 ./build_app_mac_arm.sh
 ./build_app_mac_x86.sh
 ./build_app_windows_x86.sh
+
+echo "IMPORTANT NOTE:"
+echo "ARM executable cannot be created on AMD64 platform (seems to be a bug in the appimagetool. Use actual ARM Linux to build ARM Linux executable."
