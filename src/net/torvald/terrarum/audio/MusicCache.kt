4094457ab91f116b9bed35106974b8eb551ff10b
minjaesong <alswo9628@gmail.com>
2024-04-03 00:28:11 +0900

    caching for non-ram music

--- src/net/torvald/terrarum/audio/MusicCache.kt
+++ src/net/torvald/terrarum/audio/MusicCache.kt
@@ -8,9 +8,7 @@ class MusicCache(val trackName: String) : Disposable {
     private val cache = HashMap<String, MusicContainer>()
 
     fun getOrPut(music: MusicContainer?): MusicContainer? {
-        if (music != null && !music.toRAM)
-            return music
-        if (music != null && music.toRAM) // for now only the on-the-RAM tracks are getting cached
+        if (music != null)
             return cache.getOrPut(music.name) { music.makeCopy() }
         return null
     }
