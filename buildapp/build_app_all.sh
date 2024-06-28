7582eae1ee093165227e42a7e323b7a5ed6e1cfd
minjaesong <alswo9628@gmail.com>
2024-06-28 12:58:15 +0900

    better makefile

--- buildapp/build_app_all.sh
+++ buildapp/build_app_all.sh
@@ -4,6 +4,6 @@
 ./build_app_mac_arm.sh
 ./build_app_mac_x86.sh
 ./build_app_windows_x86.sh
-
-echo "IMPORTANT NOTE:"
-echo "ARM executable cannot be created on AMD64 platform (seems to be a bug in the appimagetool. Use actual ARM Linux to build ARM Linux executable."
+./build_app_linux_arm.sh
+#echo "IMPORTANT NOTE:"
+#echo "ARM executable cannot be created on AMD64 platform (seems to be a bug in the appimagetool. Use actual ARM Linux to build ARM Linux executable."
---
+++ buildapp/make_print_description_then_exit.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+echo "run 'make assets' to create Assets directory for the building first, then"
+echo "run 'make all' to build 5 executables for the release."
+exit 0
